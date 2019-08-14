be98a20293693dd3f9f79aee1353241efdbec2bf
Aditya Raj <aditya.x510@gmail.com>
2019-08-14 23:57:28 +0530

    init

---
+++ python for ML/pyComment.py
@@ -0,0 +1,38 @@
+# showing in line comment
+print("in line comment")
+
+''' multiline
+comment 
+in 
+python 
+'''
+print("multiline comment ")
+
+print("indentation is very important in python")
+# use a tab space or use 4 space as indentation
+
+
+if True:
+    print("Machine learning")
+
+if True:
+    print("Delimeter use")
+    print("type of if statement")
+
+
+print("multiline statement")
+
+sum1 = 1+2+3 + \
+    4+5+6 + \
+    8+9
+
+print(sum1)
+
+sum2 = (1+2+3+4
+        + 5+6+7
+        + 8+9)
+
+print(sum2)
+
+a = 10; b = 20; c = 30
+print(a,b,c)
