f37c1daa12aa6818e02ce5f2527385b023e46fb1
Aditya Raj <aditya.x510@gmail.com>
2019-08-21 20:08:41 +0530

    sorted() function
    # the easiest way to sort a list is with the sorted(list) method
    # it takes a list and retun a new list with with those list element in sorted order
    # by default the order is ascending order
    # the original list is not changed.

---
+++ python for ML/listDataStructure/listSorting.py
@@ -0,0 +1,6 @@
+# the easiest way to sort a list is with the sorted(list) method
+# it takes a list and retun a new list with with those list element in sorted order
+# by default the order is ascending order
+# the original list is not changed.
+
+
