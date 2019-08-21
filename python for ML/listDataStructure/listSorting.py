324f898f3fadfa7471b899171e18a8ac0216c057
Aditya Raj <aditya.x510@gmail.com>
2019-08-21 20:13:25 +0530

    init

--- python for ML/listDataStructure/listSorting.py
+++ python for ML/listDataStructure/listSorting.py
@@ -3,4 +3,15 @@
 # by default the order is ascending order
 # the original list is not changed.
 
+num = [3, 2, 4, 1, 6, 5, 8, 7, 9, 0]
+sorted_list = sorted(num)
+print(sorted_list)
+
+# for sorting in descending order
+
+sorted_list_des = sorted(num, reverse=True)
+print(sorted_list_des)
+
+# sort function
+
 
