e8da0676f5b601b23362aafe56c491c17ca26fdb
Aditya Raj <aditya.x510@gmail.com>
2019-09-19 10:19:23 +0530

    Update graphStyling.py

--- data-Visualization/graphStyling.py
+++ data-Visualization/graphStyling.py
@@ -61,3 +61,16 @@ colors = ['b', 'r', 'g', 'y', 'c']
 plt.pie(slices_hours, labels=activities,
         colors=colors, startangle=90, autopct='%.1f%%')
 plt.show()
+
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
